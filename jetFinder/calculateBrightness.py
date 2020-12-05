385a57c971907cce470cab7f46e1c05aeca856c5
megamott <megamott27@gmail.com>
2020-12-06 01:42:50 +0300

    Added Jet and Lum storages with saver script.

--- jetFinder/calculateBrightness.py
+++ jetFinder/calculateBrightness.py
@@ -1,61 +1,91 @@
 import cv2
 import numpy as np
 import pylab
+from io import BytesIO
+from PIL import Image
+
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def showme(pic, name):
+def show_me(pic, name):
     cv2.imshow(name, pic)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
 
-jets = cv2.imread("MicroJet.jpg")
-# lab = cv2.cvtColor(jets, cv2.COLOR_BGR2LAB)
+def save_pic(name_tif):
+    png1 = BytesIO()
+    fig.savefig(png1, format='png')
 
-lum_arr = list()
+    png2 = Image.open(png1)
 
-for _ in range(jets.shape[1]):
-    row = list()
-    for _ in range(jets.shape[0]):
-        row.append(0)
-    lum_arr.append(row)
+    png2.save(name_tif + '.tiff')
 
-for x in range(jets.shape[1]):
+
+i = 0
+
+while i != 0.0650:
+    i = round(i, 4)
+    name = "I от Z/Z " + str(i) + ".jpg"
+    print(name)
+    jets = cv2.imread(name)
+    jets = jets[280:360, 760:840]
+    # lab = cv2.cvtColor(jets, cv2.COLOR_BGR2LAB)
+
+    lum_arr = list()
+
+    for _ in range(jets.shape[1]):
+        row = list()
+        for _ in range(jets.shape[0]):
+            row.append(0)
+        lum_arr.append(row)
+
+    for x in range(jets.shape[1]):
+        for y in range(jets.shape[0]):
+            b, g, r = jets[y, x]
+            lum = (0.2126 * r) + (0.7152 * g) + (0.0722 * b)
+            print("coordinates of: " + str(x) + " " + str(y) + " are: " + str(r) + " " + str(g) + " " + str(b)
+                  + " and luminance: " + str(lum))
+            lum_arr[x][y] = lum
+
+    # show_me(jets, 'lab')
+
+    x_range = []
+    y_range = []
+
+    for x in range(jets.shape[1]):
+        x_range.append(float(x))
+
     for y in range(jets.shape[0]):
-        b, g, r = jets[y, x]
-        lum = (0.2126 * r) + (0.7152 * g) + (0.0722 * b)
-        print("coordinates of: " + str(x) + " " + str(y) + " are: " + str(r) + " " + str(g) + " " + str(b)
-              + " and luminance: " + str(lum))
-        lum_arr[x][y] = lum
-
-# showme(jets, 'lab')
-
-x_range = []
-y_range = []
-
-for x in range(jets.shape[1]):
-    x_range.append(float(x))
-
-for y in range(jets.shape[0]):
-    y_range.append(float(y))
+        y_range.append(float(y))
 
 
-def makeData():
-    x = np.array(x_range)
-    y = np.array(y_range)
-    z = np.array(lum_arr)
-    xgrid, ygrid = np.meshgrid(y, x)
+    def makeData():
+        x = np.array(x_range)
+        y = np.array(y_range)
+        z = np.array(lum_arr)
+        xgrid, ygrid = np.meshgrid(y, x)
 
-    zgrid = z
-    return xgrid, ygrid, zgrid
+        zgrid = z
+        return xgrid, ygrid, zgrid
 
 
-x, y, z = makeData()
+    x, y, z = makeData()
 
-fig = pylab.figure()
-axes = Axes3D(fig)
+    fig = pylab.figure()
+    axes = Axes3D(fig)
 
-axes.plot_surface(x, y, z)
+    axes.plot_surface(x, y, z)
 
-pylab.show()
+    pylab.show()
+
+    show_me(jets, 'jets')
+
+    D_name = "Lum/" + "I " + str(i)
+    save_pic(D_name)
+
+    cv2.imwrite('Jet_pic_lum/' + 'JI ' + str(i) + '.jpg', jets)
+
+    i += 0.0025
+
+    cv2.waitKey(0)
