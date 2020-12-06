4fc305fd3a940b3ee59dcec6cf9442bb0503b623
megamott <megamott27@gmail.com>
2020-12-07 00:18:52 +0300

    Add SNR calculation script.

--- jetFinder/make_plot.py
+++ jetFinder/make_plot.py
@@ -2,11 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from jetFinder import calculate_brightness
 
-cor_arr, lum_arr = calculate_brightness.main()
 
+# cor_arr, lum_arr = calculate_brightness.main()
 
 def make_2d_plot(cor_array, lum_array):
-
     fig, ax = plt.subplots()
 
     ax.plot(cor_array, lum_array, color='b', linewidth=2)
@@ -14,8 +13,8 @@ def make_2d_plot(cor_array, lum_array):
     #  Scale intervals
     ax.xaxis.set_major_locator(ticker.MultipleLocator(0.0025))
     ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.00125))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(20))
-    ax.yaxis.set_minor_locator(ticker.MultipleLocator(10))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.05))
 
     #  Add lines of general grid
     ax.grid(which='major', color='k')
@@ -32,10 +31,11 @@ def make_2d_plot(cor_array, lum_array):
     fig.set_figheight(14)
 
     plt.xlabel('Z coordinate')
-    plt.ylabel('Luminance')
+    plt.ylabel('SNR')
     plt.show()
 
-    calculate_brightness.save_pic('P2_Z_I', fig)
+    calculate_brightness.save_pic('P3_Z_SNR', fig)
 
 
-make_2d_plot(cor_arr, lum_arr)
+# if __name__ == '__main__':
+#     make_2d_plot(cor_arr, lum_arr)
